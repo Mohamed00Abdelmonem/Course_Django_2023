f4a02a90449a505a9cc821387b391f679ebc3052
unknown <mmohamedabdelm@gmail.com>
2023-06-13 12:26:34 +0300

    q4 level

--- taskes/level 1.2/level1.2.py
+++ taskes/level 1.2/level1.2.py
@@ -1,6 +1,8 @@
 # Python Task Level 1 :
 
 
+# **********************************************************************************************
+
 
 # 1. Create a python function that takes 2 numbers x,y and prints 2 lists containing the odd and even numbers in
 # the x,y range
@@ -38,7 +40,7 @@
 
 
 
-
+# **********************************************************************************************
 
 # 2. Create a python function that takes 2 numbers x,y and prints all the numbers between 1 and 100 than can
 # be divided on x,y
@@ -61,7 +63,72 @@
 #         if i % x ==0 and i % y ==0 :
 #             value.append(i)
 #     return value
-        
-
+    
 # print(divided_by_x_and_y(3,5))
 
+
+
+
+# **********************************************************************************************
+
+
+# 3. Create a python function that takes 2 numbers x, y and prints the multiplication table from x to y
+
+    ####################### the normal way #################################
+
+
+# def multiplication_table(x,y):
+#     for i in range(1,12+1):
+#         for ii in range (x,y+1):
+#          print(f" {i} * {ii} = {i*ii}")
+        
+# multiplication_table(1,9)
+     
+
+
+
+    ################################# list comprhanchan ################################
+
+
+
+# def multiplication_table(x,y):
+#     value = [[i,ii,i*ii] for i in range(x,y+1) for ii in range (1,y+1) ]
+#     print(value)
+#     for entry in value:
+#         print(f"{entry[0]} * {entry[1]} = {entry[2]}")
+
+    
+# print(multiplication_table(2,5))
+
+
+# **********************************************************************************************
+
+
+# # 4. Create a function that takes a sentence and prints the sentence without duplicated words
+
+
+    ####################### the normal way #################################
+
+
+# def no_duplicated(sentenc):
+#     result = []
+#     words = sentenc.split(' ')
+#     for word in words:
+#         if word not in result:
+#             result.append(word)
+#             r = ' '.join(str(e) for e in result)
+#     return r
+# print(no_duplicated("aya is is aya is are you"))        
+
+
+
+    ################################# list comprhanchan ################################
+
+
+# def no_duplicated(sentence):
+#     words = sentence.split(' ')
+#     unique_words = []
+#     result_list = [word for word in words if word not in unique_words]
+#     x = " ".join(str(e) for e in result_list)
+#     return x
+# print(no_duplicated("hi mohamed hi and ahmed "))
